79009fb67d03793c5f169d88ba9c0c72dd694feb
Erizta Alifa P <33772785+erizta@users.noreply.github.com>
2021-05-27 12:22:57 +0700

    update product details page
    # fix slider preview
    # fix main content
    # fix suggestion section
    # add to cart
    # add global context

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import 'assets/css/app.css'
-import HomePage from 'pages/HomePage';
-import Details from 'pages/Details';
-import Cart from 'pages/Cart';
-import Congratulation from 'pages/Congratulation';
-import NotFound from 'pages/NotFound';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
+import HomePage from 'pages/HomePage'
+import Details from 'pages/Details'
+import Cart from 'pages/Cart'
+import Congratulation from 'pages/Congratulation'
+import NotFound from 'pages/NotFound'
+
+import Provider from 'helpers/hooks/useGlobalContext'
 
 function App() {
   return (
-    <div className="App">
+    <Provider> 
       <Router>
         <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route path="/categories/:idc" component={Details} />
+          <Route exact path="/categories/:idc" component={Details} />
+          <Route path="/categories/:idc/products/:idp" component={Details} />
           <Route path="/cart" component={Cart} />
           <Route path="/congratulation" component={Congratulation} />
           <Route path="*" component={NotFound} />
         </Switch>
       </Router>
-    </div>
+    </Provider>
   )
 }
 
-export default App;
+export default App
